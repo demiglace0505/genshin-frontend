a07cc67d98f60ac062e10eec904217fdeed0c7ee
demiglace0505 <demiglace0505@gmail.com>
2021-04-23 16:58:19 +0800

    global reset with styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
+import { GlobalStyle } from './config.js'
 import Characters from './components/Characters.js'
 import { initializeCharacters } from './reducers/characterReducer.js'
+
 
 const App = () => {
   const dispatch = useDispatch()
@@ -13,10 +15,12 @@ const App = () => {
   }, [dispatch])
 
 
-  
+
   return (
     <div>
-      <Characters />  
+      <GlobalStyle />
+      <h1>Serialize yourself</h1>
+      <Characters />
     </div>
   )
 }
--- src/config.js
+++ src/config.js
@@ -1,3 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    // defines what 1 rem is
+    //62.5/100*16 = 10px
+    font-size: 62.5%
+  } 
+
+  body {
+    box-sizing: border-box;
+    font-size: 1.6rem;
+  }
+`
+
 const URI = 'http://localhost:5000'
 
-export { URI }
+export {
+  URI,
+  GlobalStyle
+}
